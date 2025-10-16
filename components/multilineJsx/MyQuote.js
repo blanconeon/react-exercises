e27b636ac3a91a82344dc3fb6a8be557d0cae902
Emmanuel B <blancocemmanuel@gmail.com>
2025-10-17 05:20:42 +1100

    components

---
+++ components/multilineJsx/MyQuote.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Component() {
+return (
+<blockquote>
+  <p>
+    What is important now is to recover our senses.
+  </p>
+  <cite>
+    <a target="_blank" 
+      href="https://en.wikipedia.org/wiki/Susan_Sontag">
+      Susan Sontag
+    </a>
+  </cite>
+</blockquote>
+);
+};
+
+export default Component; 
