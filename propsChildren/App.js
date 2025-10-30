0e28c2733366dcbf016e1e904ffb2929ba857a79
Emmanuel B <blancocemmanuel@gmail.com>
2025-10-30 21:43:46 +1100

    propsChildren

---
+++ propsChildren/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import List from './List';
+
+function App() {
+  return (
+    <div>
+      <List type='Living Musician'>
+        <li>Sachiko M</li>
+        <li>Harvey Sid Fisher</li>
+      </List>
+      <List type='Living Cat Musician'>
+        <li>Nora the Piano Cat</li>
+         <li>Nora the Piano Tiger</li>
+      </List>
+    </div>
+  );
+}
+
+export default App;
